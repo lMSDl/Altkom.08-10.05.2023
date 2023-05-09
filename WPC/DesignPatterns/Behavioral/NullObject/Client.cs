b01092eb3f36d381cdb47782b71feea2526dc55a
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-09 14:56:52 +0200

    NullObject

---
+++ WPC/DesignPatterns/Behavioral/NullObject/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.NullObject
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var objects = new AbstractObject[] { new RealObject(), new NullObject()};
+
+            var random = new Random();
+            while (true)
+            {
+                var value = random.Next(0, objects.Length);
+                Console.ReadLine();
+
+                /*if (value == null)
+                    continue;*/
+
+                objects[value].Method1();
+                objects[value].Method2();
+                objects[value].Method3();
+            }
+
+        }
+    }
+}
---
+++ WPC/DesignPatterns/Behavioral/NullObject/NullObject.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.NullObject
+{
+    public class NullObject : AbstractObject
+    {
+        public new void Method1()
+        {
+
+        }
+
+        public override void Method2()
+        {
+        }
+
+        public override void Method3()
+        {
+
+        }
+    }
+}
