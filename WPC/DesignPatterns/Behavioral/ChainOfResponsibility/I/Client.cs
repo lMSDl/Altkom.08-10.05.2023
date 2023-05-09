dfae8262e8431f521b743938e277f0c09cb22681
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-09 14:43:36 +0200

    ChainOfResponsibility

---
+++ WPC/DesignPatterns/Behavioral/ChainOfResponsibility/I/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.ChainOfResponsibility.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var handler4 = new DiscountHandler(1000, 6000, "Kierownik sklepu");
+            var handler3 = new DiscountHandler(1000, 7000, "Kierownik sali", handler4);
+            //var handler2 = new DiscountHandler(500, 9000, "Kierownik kas", handler3);
+            var handler1 = new DiscountHandler(100, 10000, "Kasjer", handler3);
+
+
+            var result = handler1.Discount(500, 15000);
+            if(!result)
+                Console.WriteLine("Nie otrzymano rabatu");
+
+
+
+            result = handler3.Discount(600, 8000);
+            if (!result)
+                Console.WriteLine("Nie otrzymano rabatu");
+        }
+        }
+}
---
+++ WPC/DesignPatterns/Behavioral/ChainOfResponsibility/I/DiscountHandler.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.ChainOfResponsibility.I
+{
+    public class DiscountHandler : IDiscountHandler
+    {
+        public float MaxDiscount { get; }
+        public float MinPrice { get; }
+
+        private IDiscountHandler _nextHandler;
+        public string Name { get; }
+
+        public DiscountHandler(float maxDiscount, float minPrice, string name, IDiscountHandler nextHandler) : this(maxDiscount, minPrice, name)
+        {
+            _nextHandler = nextHandler;
+        }
+
+        public DiscountHandler(float maxDiscount, float minPrice, string name)
+        {
+            MaxDiscount = maxDiscount;
+            MinPrice = minPrice;
+            Name = name;
+        }
+
+        public bool Discount(float discountValue, float productsPrice)
+        {
+            if(MinPrice <= productsPrice && discountValue <= MaxDiscount)
+            {
+                Console.WriteLine($"{Name} udzielił rabatu - {discountValue} zł");
+                return true;
+            }
+            return _nextHandler?.Discount(discountValue, productsPrice) ?? false;
+        }
+    }
+}
