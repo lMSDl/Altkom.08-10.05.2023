c9de0e042b1899f026bdc4097e6892ef4264e4eb
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-10 10:53:57 +0200

    Memento

---
+++ WPC/DesignPatterns/Behavioral/Memento/Person.cs
@@ -0,0 +1,50 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.Memento
+{
+    internal class Person : ICloneable, IRestorable<Person>, INotifyPropertyChanged
+    {
+        private string firstName;
+        private string lastName;
+
+        public string FirstName
+        {
+            get => firstName; set
+            {
+                firstName = value;
+                PropertyChanged?.Invoke(this, null);
+            }
+        }
+        public string LastName
+        {
+            get => lastName; set
+            {
+                lastName = value;
+                PropertyChanged?.Invoke(this, null);
+            }
+        }
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public object Clone()
+        {
+            return MemberwiseClone();
+        }
+
+        public void Restore(Person state)
+        {
+            FirstName = state.FirstName;
+            LastName = state.LastName;
+        }
+        public override string ToString()
+        {
+            return JsonConvert.SerializeObject(this);
+        }
+    }
+}
