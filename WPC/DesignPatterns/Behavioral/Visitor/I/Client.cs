822c8a22b6da08aafdb92fedadbf177ba1368568
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-10 14:27:49 +0200

    Visitor

---
+++ WPC/DesignPatterns/Behavioral/Visitor/I/Client.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.Visitor.I
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var collection = new IElement[]
+            {
+                new PlainText() {Text = "Plain"},
+                new PlainText() { Text = "\n" },
+                new BoldText() {Text = "Bold"},
+                new PlainText() { Text = "\n" },
+                new Hyperlink() {Text = "Hyperlink",  Link=@"https:\\some.url.pl"},
+            };
+
+            var visitor = new HtmlVisitor();
+            foreach (var item in collection)
+            {
+                item.Accept(visitor);
+            }
+
+            Console.WriteLine(  visitor.ToString() );
+        }
+    }
+}
---
+++ WPC/DesignPatterns/Behavioral/Visitor/II/Client.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WPC.DesignPatterns.Behavioral.Visitor.II
+{
+    internal class Client
+    {
+        public static void Execute()
+        {
+            var products = new List<Product> { new Product(), new BoxedProduct() };
+            foreach (var item in products)
+            {
+                item.PrintType();
+            }
+
+            var baskets = new List<Basket> { new Basket(), new Cart() };
+            foreach (var basket in baskets)
+            {
+                foreach (var product in products)
+                {
+                    //basket.Add(product);
+                    product.Accept(basket);
+                }
+            }
+        }
+    }
+}
