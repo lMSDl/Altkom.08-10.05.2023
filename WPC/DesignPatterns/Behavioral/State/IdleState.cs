41ffa3ba21fa75e3a9cc7da55e0ea827169063e1
Paweł Łęczycki <pawel@msd.info.pl>
2023-05-10 12:34:59 +0200

    State

---
+++ WPC/DesignPatterns/Behavioral/State/IdleState.cs
@@ -0,0 +1,21 @@
+﻿namespace WPC.DesignPatterns.Behavioral.State
+{
+    internal class IdleState : State
+    {
+        public IdleState(CoffeeMachine coffeeMachine) : base(coffeeMachine)
+        {
+        }
+
+        public override void Large()
+        {
+            System.Console.WriteLine("Duża kawa");
+            CoffeeMachine.State = new WorkingState(CoffeeMachine, 7000);
+        }
+
+        public override void Small()
+        {
+            System.Console.WriteLine("Mała kawa");
+            CoffeeMachine.State = new WorkingState(CoffeeMachine, 4000);
+        }
+    }
+}
---
+++ WPC/DesignPatterns/Behavioral/State/WorkingState.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading;
+
+namespace WPC.DesignPatterns.Behavioral.State
+{
+    internal class WorkingState : State
+    {
+        private readonly CancellationTokenSource _cancellationTokenSource;
+        public WorkingState(CoffeeMachine coffeeMachine, int time) : base(coffeeMachine)
+        {
+            _cancellationTokenSource = new CancellationTokenSource();
+
+            Task.Delay(time, _cancellationTokenSource.Token)
+                .ContinueWith(x =>
+                {
+                    _cancellationTokenSource.Dispose();
+                    coffeeMachine.State = new IdleState(coffeeMachine);
+                });
+        }
+
+        public override void Large()
+        {
+            System.Console.WriteLine("Anulacja");
+            _cancellationTokenSource.Cancel();
+        }
+
+        public override void Small()
+        {
+            System.Console.WriteLine("Anulacja");
+            _cancellationTokenSource.Cancel();
+        }
+    }
+}
